b1c86538e363f39362d0f6de90afe9588b8093a2
joaokeuwe <joaov2872@hotmail.com>
2022-11-01 22:13:03 -0300

    feat: :sparkles: criando componente com input e botão principal, e começando a logica para o botão

---
+++ src/components/InputButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <h2 style="margin-top: 200px">Tarefas</h2>
+    <input
+      type="text"
+      placeholder="digite sua tarefa aqui!"
+    />
+    <button type="button" v-on:click="send_task" >Adicionar</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "InputButtoon",
+  data() {
+    return {
+    //   inputTasks: "",
+    }
+  },
+  methods: {
+    send_task(e) {
+        e.preventDefault();
+        console.log('caiu aqui')
+    }
+  }
+};
+</script>
