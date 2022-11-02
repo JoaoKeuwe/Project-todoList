25849032189b2278e7329dd9125fd21ae2b49e70
joaokeuwe <joaov2872@hotmail.com>
2022-11-01 21:41:59 -0300

    feat: iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "./components/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <img src="../img/logo.png" alt="">
+    <li>Home</li>
+    <li>Histórico</li>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      nome: "keuwe",
+    };
+  },
+};
+</script>
