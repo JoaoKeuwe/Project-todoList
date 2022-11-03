e0e9c440eb831ce4d4d40289bc7916a9bb8523fe
joaokeuwe <joaov2872@hotmail.com>
2022-11-02 21:13:01 -0300

    refactor: :fire: refatorando Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,16 @@
 <template>
-    <Header />
-    <InputButtonVue />
+  <Header />
+  <InputButtonVue />
 </template>
 
 <script>
-import Header from '../components/Header.vue'
-import InputButtonVue from '../components/InputButton.vue';
-    export default {
-       name: 'Home',
-       components: {
-       Header,
-       InputButtonVue,
-    }
-    }
+import Header from "../components/Header.vue";
+import InputButtonVue from "../components/InputButton.vue";
+export default {
+  name: "Home",
+  components: {
+    Header,
+    InputButtonVue,
+  },
+};
 </script>
